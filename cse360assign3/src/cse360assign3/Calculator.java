a541085595eab44333c5e2fa45cd39b0c15eb493
Peng Zhang <pppzhang@gmail.com>
2016-02-14 23:47:15 -0700

    Javadoc has been added

---
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,56 @@
+
+package cse360assign3;
+
+/**
+ * The class Calculator is designed to do basic arithmetic operations including add, subtract, multiply and divide.
+ * @author Peng Zhang (PIN 910)
+ *
+ */
+public class Calculator {
+
+	private int total;
+	
+	/**
+	 * The default constructor does not take input. 
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * This method returns the result.
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	/**
+	 * This method adds input value to the result. 
+	 */
+	public void add (int value) {
+		
+	}
+	/**
+	 * This method subtracts input value from the result.
+	 */
+	public void subtract (int value) {
+		
+	}
+	/**
+	 * This method multiplies the result by the input value.
+	 */
+	public void multiply (int value) {
+		
+	}
+	/**
+	 * This method divide the result by the input value.
+	 */
+	public void divide (int value) {
+		
+	}
+	/**
+	 * This method returns the operation histories.
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
